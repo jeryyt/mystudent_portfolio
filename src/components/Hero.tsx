a1d1123977a14a3c265dc7c75f9d08713de0a362
Arfelo Reed Torralba <CPAMaster1994@gmail.com>
2024-10-11 20:55:02 +0800

    optimized hero section to prevent lag on loading

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,30 +1,7 @@
 import { useState, useEffect } from "react";
 import { motion, Variants } from "framer-motion";
-import { ReactTyped } from "react-typed";
 import { FaDownload } from "react-icons/fa";
 
-const textVariants: Variants = {
-  initial: {
-    x: -500,
-    opacity: 0,
-  },
-  animate: {
-    x: 0,
-    opacity: 1,
-    transition: {
-      duration: 1,
-      staggerChildren: 0.2,
-    },
-  },
-  scrollBtn: {
-    opacity: 0,
-    y: 10,
-    transition: {
-      duration: 2,
-      repeat: Infinity,
-    },
-  },
-};
 const sliderVariants: Variants = {
   initial: {
     x: 0,
@@ -45,7 +22,7 @@ const Hero = () => {
   useEffect(() => {
     const timer = setTimeout(() => {
       setShowTyped(true); // start typing animation after 500ms delay
-    }, 1500); // adjust this delay as needed
+    }, 2000); // adjust this delay as needed
 
     return () => clearTimeout(timer); // cleanup the timer
   }, []);
@@ -65,79 +42,55 @@ const Hero = () => {
   return (
     <div className="hero-container relative overflow-hidden">
       <div className="relative z-10 m-auto h-full max-w-7xl">
-        <motion.div
+        <div
           className="flex h-full w-1/2 flex-col justify-center gap-10 max-lg:mt-12 max-lg:h-1/2
             max-lg:w-full max-lg:items-center max-lg:gap-5 max-lg:text-center"
-          variants={textVariants}
-          initial="initial"
-          animate="animate"
-          style={{ willChange: "transform" }}
         >
-          <motion.h2
-            className="text-3xl tracking-[10px] max-lg:text-2xl"
-            variants={textVariants}
-            style={{ willChange: "transform" }}
-          >
+          <h2 className="text-3xl tracking-[10px] max-lg:text-2xl">
             Hi I'm <span className="text-purple-800">Reed</span>
-          </motion.h2>
+          </h2>
 
-          {showTyped && (
-            <motion.h1
-              className="text-[88px] font-bold tracking-wider max-lg:text-5xl"
-              variants={textVariants}
-              style={{ willChange: "transform" }}
-            >
-              <ReactTyped
-                strings={["Full Stack", "Front End", "Back End"]}
-                typeSpeed={50}
-                backDelay={800}
-                backSpeed={50}
-                loop
-              />{" "}
-              <br />
-              Developer
-            </motion.h1>
-          )}
+          <h1 className="text-[88px] font-bold tracking-wider max-lg:text-5xl">
+            Full Stack
+            <br />
+            Developer
+          </h1>
 
-          <motion.div
-            variants={textVariants}
-            className="flex items-center max-lg:mt-5"
-            style={{ willChange: "transform" }}
-          >
+          <div className="flex items-center max-lg:mt-5">
             {/* download cv */}
-            <motion.a
+            <a
               href="/resume.pdf" // file in public dir
               download="Reed_Resume.pdf"
               className="mr-5 flex cursor-pointer items-center gap-2 rounded-md border border-white
                 bg-yellow-600 p-5 font-light text-white transition ease-out hover:bg-transparent"
-              variants={textVariants}
-              style={{ willChange: "transform" }}
             >
               <FaDownload /> Download my CV
-            </motion.a>
-            <motion.button
+            </a>
+            <button
               className="rounded-md border border-white bg-transparent p-5 font-light text-white
                 transition ease-out hover:bg-gray-400 hover:text-black"
-              variants={textVariants}
               onClick={() => handleClick("Contact")}
-              style={{ willChange: "transform" }}
             >
               Contact Me
-            </motion.button>
-          </motion.div>
+            </button>
+          </div>
 
-          {showTyped && (
-            <motion.img
-              src="/scroll.png"
-              alt="scolldown image"
-              className="w-[50px]"
-              variants={textVariants}
-              animate="scrollBtn"
-              loading="lazy"
-              style={{ willChange: "transform" }}
-            />
-          )}
-        </motion.div>
+          <motion.img
+            src="/scroll.png"
+            alt="scolldown image"
+            className="w-[50px]"
+            animate={{
+              opacity: 0,
+              y: 10,
+              transition: {
+                duration: 3,
+                repeat: Infinity,
+              },
+            }}
+            loading="lazy"
+            style={{ willChange: "transform" }}
+          />
+        </div>
       </div>
 
       {showTyped && (
@@ -162,7 +115,6 @@ const Hero = () => {
           alt="Reed's picture"
           className="size-full object-cover"
           loading="lazy"
-          style={{ willChange: "transform" }}
         />
       </div>
     </div>
